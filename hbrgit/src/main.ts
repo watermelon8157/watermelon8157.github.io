fa6b583b134b790d68b79322752660db5369cab8
watermelon <watermelon8175@gmail.com>
2024-03-11 20:24:16 +0800

    build

--- hbrgit/src/main.ts
+++ hbrgit/src/main.ts
@@ -1,4 +1,10 @@
-import './assets/main.css'
+import 'bootstrap'
+import "bootswatch/dist/cyborg/bootstrap.min.css";
+// setting fotawesome to avoid inline style
+import '@fortawesome/fontawesome-svg-core/styles.css'; // import usable styles manual
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; // disable auto add css for fontawesome
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -10,5 +16,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
--- hbrgit/src/views/HomeView.vue
+++ hbrgit/src/views/HomeView.vue
@@ -1,9 +1,78 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+import { ref } from "vue";
+import charactersJson from "@/assets/charactersCard/characters.json";
+import { findIndex } from "lodash-es";
+const characters = ref(charactersJson);
+const tierA = ref(false);
+const tierS = ref(false);
+const tierSS = ref(true);
+const WifeCardList = ref([]);
 
+function showCards(pTier: any) {
+  if (pTier.tier === "A" && tierA.value) {
+    return true;
+  }
+  if (pTier.tier === "S" && tierS.value) {
+    return true;
+  }
+  if (pTier.tier === "SS" && tierSS.value) {
+    return true;
+  }
+  return false;
+}
+function showCard(pCard: any) {
+  const index = findIndex(WifeCardList.value, (o: any) => o === pCard.image);
+  return index >= 0 ? "grayscale(0%)" : "grayscale(100%)";
+}
+function showCardCount(pCard: any) {
+  const index = findIndex(WifeCardList.value, (o: any) => o === pCard.image);
+  return index >= 0 ? "bg-danger" : "bg-secondary";
+}
+</script>
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="d-flex flex-row bd-highlight mb-3">
+    <div class="mx-2">Tier :</div>
+    <div class="mx-2 form-check form-switch">
+      <input class="form-check-input" type="checkbox" v-model="tierA" id="tierA" />
+      <label class="form-check-label" for="tierA">A</label>
+    </div>
+    <div class="mx-2 form-check form-switch">
+      <input class="form-check-input" type="checkbox" v-model="tierS" id="tierS" />
+      <label class="form-check-label" for="tierS">S</label>
+    </div>
+    <div class="mx-2 form-check form-switch">
+      <input class="form-check-input" type="checkbox" v-model="tierSS" id="tierSS" />
+      <label class="form-check-label" for="tierSS">SS</label>
+    </div>
+  </div>
+  <div class="container-fluid">
+    <template v-for="Wife in characters" :key="'Wife' + Wife.id">
+      <template v-for="WifeCards in Wife.cards" :key="'WifeCards' + WifeCards.id">
+        <span v-if="showCards(WifeCards)" class="m-2">
+          <input
+            class="d-none"
+            type="checkbox"
+            v-model="WifeCardList"
+            :id="WifeCards.image"
+            :value="WifeCards.image"
+          />
+          <label class="form-check-label position-relative" :for="WifeCards.image">
+            <img
+              :src="'charactersCard/' + WifeCards.image"
+              :style="{ filter: showCard(WifeCards) }"
+            />
+            <!-- <span class="position-absolute bottom-0 end-0">
+              <span
+                class="badge rounded-pill m-2"
+                :class="[showCardCount(WifeCards)]"
+                style="font-size: 1rem"
+              >
+                0
+              </span>
+            </span> -->
+          </label>
+        </span>
+      </template>
+    </template>
+  </div>
 </template>
