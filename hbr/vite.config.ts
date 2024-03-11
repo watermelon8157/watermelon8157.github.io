df51b76642f383329dd52820bd0c707cd1082467
watermelon <watermelon8175@gmail.com>
2024-03-11 13:24:52 +0800

    build

--- hbr/vite.config.ts
+++ hbr/vite.config.ts
@@ -1,18 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+export default defineConfig(({ command, mode }) => {
+  // 根据当前工作目录中的 `mode` 加载 .env 文件
+  // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。\
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: env.NODE_ENV === "production" ? env.VITE_WEB : "/",
+  };
+});
