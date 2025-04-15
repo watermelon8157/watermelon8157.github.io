67006ba71ce38d67e724986c2382bda79cfc0841
watermelon <watermelon8175@gmail.com>
2025-04-15 16:44:12 +0800

    效果更新

--- Gallery/assets/tgaTextureLoader-coejFAqc.js
+++
@@ -1,1 +0,0 @@
-import{i as r,j as n}from"./index-C7kiCWn6.js";class d{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,o){const s=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),a=r(s);o(a.width,a.height,t.generateMipMaps,!1,()=>{n(t,s)})}}export{d as _TGATextureLoader};
